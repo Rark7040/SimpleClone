d345e73ecc2d67884780386a237d2ca25b275b20
Rark7040 <crimera.hello@gmail.com>
2021-05-19 10:03:16 +0900

    PlayerQuitEventHandler仮完成

--- src/rark/simple_clone/event/handler/PlayerQuitEventHandler.php
+++ src/rark/simple_clone/event/handler/PlayerQuitEventHandler.php
@@ -4,13 +4,21 @@ declare(strict_types = 1);
 
 namespace rark\simple_clone\event\handler;
 
-
 use pocketmine\event\{
-	Event
+	Event,
+	player\PlayerQuitEvent
 };
+use rark\simple_clone\{
+	Main,
+	utils\Structure
+};
+
 
 final class PlayerQuitEventHandler implements Handler{
 
 	public static function recive(Event $event):void{
+		if(!$event instanceof PlayerQuitEvent) return;
+		if(!Main::getConfig()->get('quit.refresh')) return;
+		Structure::refreshHistoryCash($event->getPlayer()->getName());
 	}
 }
